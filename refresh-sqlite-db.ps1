9f4e970468a17d35fcc04c1fa370c3de1439fbeb
drminnaar <douglas.minnaar@gmail.com>
2019-10-26 12:07:59 +1300

    Amend sqlite setup script to accomodate working with cross-platform powershell

--- refresh-sqlite-db.ps1
+++ refresh-sqlite-db.ps1
@@ -5,11 +5,25 @@
 }
 
 $projectPath = Get-Location
-$sqliteProjectPath = "$($projectPath)\Identity.Database.Sqlite\Migrations"
-$sqliteDbPath = "$($projectPath)\data"
+$sqliteMigrationsPath = "$($projectPath)/Identity.Database.Sqlite/Migrations"
+$sqliteDbPath = "$($projectPath)/Identity.Database.Sqlite/identity.db"
 
 $currentForegroundColor = Set-ConsoleForegroundColor "Green"
 Write-Host "`r`nScript started! Creating new Identity database using Sqlite."
+
+# Remove old data and migrations
+Set-ConsoleForegroundColor Yellow | Out-Null
+Write-Host "`r`n"
+
+if (Test-Path -Path $sqliteDbPath) {
+    Remove-Item -Path $sqliteDbPath
+    Write-Host "Removed Sqlite Db '$($sqliteDbPath)'"
+}
+
+if (Test-Path -Path $sqliteMigrationsPath) {
+    Remove-Item -Recurse -Path $sqliteMigrationsPath
+    Write-Host "Removed Sqlite database migrations folder '$($sqliteMigrationsPath)'"
+}
 
 # Clean, Restore and Build solution
 Set-ConsoleForegroundColor White | Out-Null
@@ -20,34 +34,15 @@ dotnet restore
 dotnet build --no-restore --configuration Release
 Write-Host "Completed building solution"
 
-# Remove old data and migrations
-Set-ConsoleForegroundColor Yellow | Out-Null
-Write-Host "`r`n"
-
-if (Test-Path -Path $sqliteDbPath) {
-    Remove-Item -Recurse -Path $sqliteDbPath
-    Write-Host "Removed Sqlite Db folder '$($sqliteDbPath)'"
-}
-
-if (Test-Path -Path $sqliteProjectPath) {
-    Remove-Item -Recurse -Path $sqliteProjectPath
-    Write-Host "Removed Sqlite database migrations folder '$($sqliteProjectPath)'"
-}
-
-Write-Host "`r`nCreating Sqlite Db folder '$($sqliteDbPath)'"
-New-Item -Path $sqliteDbPath -ItemType Directory | Out-Null
-Write-Host "Created Sqlite Db folder '$($sqliteDbPath)'."
-
 # Create Sqlite migrations and update database
 Set-ConsoleForegroundColor Magenta | Out-Null
-
 Write-Host "`r`nCreating initial Sqlite Identity database migration"
-dotnet-ef migrations add --startup-project .\Identity.Database.Sqlite CreateInitialIdentitySchema
+dotnet-ef migrations add --startup-project ./Identity.Database.Sqlite CreateInitialIdentitySchema
 Write-Host "Created initial Sqlite Identity database migration"
 
 Set-ConsoleForegroundColor DarkMagenta | Out-Null
 Write-Host "`r`nStarting Sqlite Identity database migrations"
-dotnet-ef database update --startup-project .\Identity.Database.Sqlite
+dotnet-ef database update --startup-project ./Identity.Database.Sqlite
 Write-Host "Completed Sqlite Identity database migrations"
 
 # Seed Identity database
